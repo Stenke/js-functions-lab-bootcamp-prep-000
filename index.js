a3d3e49bc659bfcd22b236a2339f9c3a5f860754
Stephen Enke <stephenenke@gmail.com>
2020-08-17 21:18:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  
+}
+function happyHolidaysTo() {
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyCustomHolidayTo() {
+  console.log(`Happy ${hoiday}, ${name}`)
+}
+function holidayCountdown() {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
