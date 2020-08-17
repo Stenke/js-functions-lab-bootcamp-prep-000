ad11bfba2bbcf2b3056ebed4dc3796642f0b1288
Stephen Enke <stephenenke@gmail.com>
2020-08-17 21:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  name = "you"
   console.log(`Happy holidays, ${name}`)
 }
 function happyCustomHolidayTo(holiday, name) {
